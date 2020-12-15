37b3c5020fd67c61945266e3753fe4bb55c4e97f
Dayana-Puetate <yeceniapuetate2000@hotmail.com>
2020-12-15 17:48:33 -0500

    Metodo Division

--- src/operaciones2/Operaciones2.java
+++ src/operaciones2/Operaciones2.java
@@ -1,37 +1,40 @@
-
 package operaciones2;
 
 import java.util.Scanner;
 
-
 public class Operaciones2 {
-    public int suma(int num1, int num2){
-        return num1+num2;
+
+    public int suma(int num1, int num2) {
+        return num1 + num2;
     }
-    
-    public int resta(int num1, int num2 ){
-        return num1-num2;
+
+    public int resta(int num1, int num2) {
+        return num1 - num2;
     }
+
     //////////Metodo Multiplicación
-  public int multiplicacion(int num1, int num2 ){
-        return num1*num2;
+    public int multiplicacion(int num1, int num2) {
+        return num1 * num2;
     }
-    
-    
-    public void ifactorial(){
+
+    public void ifactorial() {
         int factorial = 1;
         int numero;
         Scanner teclado = new Scanner(System.in);
         numero = Integer.valueOf(teclado.nextLine());
-        while ( numero!=0) {
-        factorial=factorial*numero;
-        numero--;
+        while (numero != 0) {
+            factorial = factorial * numero;
+            numero--;
         }
     }
-    
-    public int raiz(int numero){
+
+    public int raiz(int numero) {
         int resultado = (int) (float) Math.sqrt(numero);
         return numero;
     }
-    
+
+    public double division(double num1, double num2) {
+        double resultado = num1 / num2;
+        return resultado;
+    }
 }
