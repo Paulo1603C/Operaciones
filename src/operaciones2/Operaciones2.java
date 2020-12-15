5ca2649ccff36f63ab014bb235a63895b0ec03f3
PAULO <paulomartinez1999@gmail.com>
2020-12-14 21:23:22 -0500

    Proyecto creado conmetodo suma y resat

---
+++ src/operaciones2/Operaciones2.java
@@ -0,0 +1,16 @@
+
+package operaciones2;
+
+
+public class Operaciones2 {
+
+    public int suma(int num1, int num2){
+        return num1+num2;
+    }
+    
+    public int resta(int num1, int num2 ){
+        return num1-num2;
+    }
+    
+    
+}
