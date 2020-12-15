61d2f0ed3a83e08d9c9df68fdc931aabc855a6c7
Lizbeth42 <74006880+Lizbeth42@users.noreply.github.com>
2020-12-15 15:44:46 -0500

    Update Operaciones2.java
    "Método para la Multiplicación"

--- src/operaciones2/Operaciones2.java
+++ src/operaciones2/Operaciones2.java
@@ -12,6 +12,11 @@ public class Operaciones2 {
     public int resta(int num1, int num2 ){
         return num1-num2;
     }
+    //////////Metodo Multiplicación
+  public int multiplicacion(int num1, int num2 ){
+        return num1*num2;
+    }
+    
     
     public void ifactorial(){
         int factorial = 1;
