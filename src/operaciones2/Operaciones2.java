0e3cc0eb366786eb033a4ff5c07efabb47c6d4bf
DiegoCata <difercata26@gmail.com>
2020-12-15 00:16:34 -0500

    Metodo Factorial

--- src/operaciones2/Operaciones2.java
+++ src/operaciones2/Operaciones2.java
@@ -1,8 +1,14 @@
 
 package operaciones2;
 
+import java.util.Scanner;
+
 
 public class Operaciones2 {
+    public static void main(String[] args) {
+        System.out.println(suma());
+        
+    }
 
     public int suma(int num1, int num2){
         return num1+num2;
@@ -12,5 +18,15 @@ public class Operaciones2 {
         return num1-num2;
     }
     
+    public void ifactorial(){
+        int factorial = 1;
+        int numero;
+        Scanner teclado = new Scanner(System.in);
+        numero = Integer.valueOf(teclado.nextLine());
+        while ( numero!=0) {
+        factorial=factorial*numero;
+        numero--;
+        }
+    }
     
 }
